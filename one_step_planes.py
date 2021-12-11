cc883daf4ea7c389614829e38a33501cd894c116
nx <nishida@rnum.org>
2021-12-11 18:53:37 +0900

    First working commit.

---
+++ one_step_planes.py
@@ -0,0 +1,34 @@
+# Precondition: SOLIDWORKS opened. One seed refplane & one or more points selected.
+
+import sys
+import win32com.client
+
+swApp = win32com.client.Dispatch("SLDWORKS.Application")
+swModel = swApp.ActiveDoc
+selMgr = swModel.SelectionManager
+featureMgr = swModel.FeatureManager
+
+ref_plane_is_selected = False
+seed_sketch_points = []
+for i, dummy in enumerate(range(selMgr.GetSelectedObjectCount2(-1)),1):
+    if selMgr.GetSelectedObjectType3(i, -1) == 4: #swSelDATUMPLANES
+        if ref_plane_is_selected:
+            print("Choose only one plane. Program aborted.")
+            sys.exit()
+        ref_plane_is_selected = True
+        seed_ref_plane = selMgr.GetSelectedObject6(i, -1)
+    elif selMgr.GetSelectedObjectType3(i, -1) == 25: #swSelEXTSKETCHPOINTS
+        seed_sketch_points.append(selMgr.GetSelectedObject6(i, -1))
+
+# Terminate program if necessary plane and points are not selected.
+if not ref_plane_is_selected or len(seed_sketch_points) == 0:
+    print("One plane and one or more points selection required for this function. Program aborted.")
+    sys.exit()
+
+# Insert reference plane(s)
+for p in seed_sketch_points:
+    seed_ref_plane.Select2(False, 0) # False: means re-select after selection clear
+    p.Select2(True, 1)
+    featureMgr.InsertRefPlane(1, 0, 4, 0, 0, 0)
+
+swModel.ClearSelection2(True)
